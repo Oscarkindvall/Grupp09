41c89eb0ee33629f833bcc9dd72ce92946d1eac9
Filip-M <filip.moltzer@gmail.com>
2020-05-07 02:38:29 +0200

    New BookMatch design

--- python/static/js/app.js
+++ python/static/js/app.js
@@ -2,5 +2,25 @@ const goBack = function() {
     window.history.back();
   }
   document.getElementById("backBtn").onclick = function (){
-    goBack();
+    goBack();  
   }
+  
+  players = document.querySelectorAll(".players")
+
+  const change = function() {
+    if (this.classList.contains("booked")) {
+      alert("already booked");
+      console.log("already booked");
+    }
+    else {
+      this.src = '/static/img/user (2).png';
+      this.classList.add("booked");
+    }
+  }
+
+  for (i=0; i<4; i++) {
+    players[i].onclick = change
+  }
+
+ 
+
